6d7fa82bb74da5e047e06c758863b80146e00704
ShawnShaddix <glushkovaevgeniia@gmail.com>
2024-07-22 16:38:39 +0200

    grabbing system implemented

--- Panacea/Source/Panacea/GrabbingSystemComponent.cpp
+++ Panacea/Source/Panacea/GrabbingSystemComponent.cpp
@@ -1,34 +1,130 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GrabbingSystemComponent.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/InputComponent.h"
 
 // Sets default values for this component's properties
 UGrabbingSystemComponent::UGrabbingSystemComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+    PrimaryComponentTick.bCanEverTick = true;
 }
-
 
 // Called when the game starts
 void UGrabbingSystemComponent::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	// ...
-	
+    FindPhysicsHandle();
+    SetupInputComponent();
 }
-
 
 // Called every frame
 void UGrabbingSystemComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+    // If the physics handle is attached
+    if (PhysicsHandle && PhysicsHandle->GrabbedComponent)
+    {
+        // Move the object that we're holding
+        PhysicsHandle->SetTargetLocation(GetReachLineEnd());
+    }
 }
 
+void UGrabbingSystemComponent::Grab()
+{
+    if (!PhysicsHandle) { return; }
+
+    if (PhysicsHandle->GrabbedComponent)
+    {
+        // If we're currently grabbing something, release it
+        UE_LOG(LogTemp, Warning, TEXT("Grab released"));
+        PhysicsHandle->ReleaseComponent();
+    }
+    else
+    {
+        // LINE TRACE and see if we reach any actors with physics body collision channel set
+        UE_LOG(LogTemp, Warning, TEXT("Grab pressed"));
+        auto HitResult = GetFirstPhysicsBodyInReach();
+        auto ComponentToGrab = HitResult.GetComponent(); // gets the mesh in our case
+        auto ActorHit = HitResult.GetActor();
+
+        // If we hit something and it has the "Ingredient" tag, then attach a physics handle
+        if (ActorHit && ActorHit->ActorHasTag("Ingredient"))
+        {
+            PhysicsHandle->GrabComponentAtLocation(
+                ComponentToGrab,
+                NAME_None, // no bones needed
+                ComponentToGrab->GetOwner()->GetActorLocation()
+            );
+        }
+    }
+}
+
+
+void UGrabbingSystemComponent::FindPhysicsHandle()
+{
+    PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+    if (PhysicsHandle == nullptr)
+    {
+        UE_LOG(LogTemp, Error, TEXT("%s missing physics handle component"), *GetOwner()->GetName());
+    }
+}
+
+void UGrabbingSystemComponent::SetupInputComponent()
+{
+    InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+    if (InputComponent)
+    {
+        InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabbingSystemComponent::Grab);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("%s missing input component"), *GetOwner()->GetName());
+    }
+}
+
+const FHitResult UGrabbingSystemComponent::GetFirstPhysicsBodyInReach()
+{
+    // Line-trace (AKA ray-cast) out to reach distance
+    FHitResult HitResult;
+    FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+    GetWorld()->LineTraceSingleByObjectType(
+        OUT HitResult,
+        GetReachLineStart(),
+        GetReachLineEnd(),
+        FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+        TraceParams
+    );
+
+    return HitResult;
+}
+
+FVector UGrabbingSystemComponent::GetReachLineStart()
+{
+    // Get player viewpoint
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+        OUT PlayerViewPointLocation,
+        OUT PlayerViewPointRotation
+    );
+
+    return PlayerViewPointLocation;
+}
+
+FVector UGrabbingSystemComponent::GetReachLineEnd()
+{
+    // Get player viewpoint
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+        OUT PlayerViewPointLocation,
+        OUT PlayerViewPointRotation
+    );
+
+    // Draw a line from the player showing the reach
+    return PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+}
