2e5e586b2401b41563d5d84e14afcbaeacd6e220
Laurel Parsons <34589706+LaurelParsons@users.noreply.github.com>
2024-07-25 10:34:56 +0200

    Unessesary assets deleted and folders restructured

--- Panacea/Source/Panacea/PanaceaGameMode.cpp
+++ Panacea/Source/Panacea/PanaceaGameMode.cpp
@@ -8,7 +8,7 @@ APanaceaGameMode::APanaceaGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/BP_FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	OnIngredientAdded.AddDynamic(this, &APanaceaGameMode::RecordIngredient);
