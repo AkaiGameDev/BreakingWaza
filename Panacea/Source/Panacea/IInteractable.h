e2856f7f935cee9a66d8e31c103ed5d09c1b793f
Oskar Kalbarczyk <106467480+okdotdev@users.noreply.github.com>
2024-07-26 16:34:00 +0200

    Make Interactable Callable in Blue Print And Clean Objects In Test Room

--- Panacea/Source/Panacea/IInteractable.h
+++ Panacea/Source/Panacea/IInteractable.h
@@ -23,6 +23,6 @@ class PANACEA_API IInteractable
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 
-
+	UFUNCTION(BlueprintCallable, Category = "Interactable")
 	virtual void Interact() = 0;
 };
