521ee37b03fe2d03df8864cd20097068231f709c
Oleksandr Smirnov <116755622+oophiuchuss@users.noreply.github.com>
2024-08-06 10:23:32 +0200

    Update Circle class

--- Panacea/Source/Panacea/AlchemyCircleActor.cpp
+++ Panacea/Source/Panacea/AlchemyCircleActor.cpp
@@ -3,3 +3,23 @@
 
 #include "AlchemyCircleActor.h"
 
+AAlchemyCircleActor::AAlchemyCircleActor()
+{
+	// Create and set up a sphere component as the root component
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComponent"));
+	RootComponent = CollisionComponent;
+
+	// Set up the collision parameters
+	CollisionComponent->SetCollisionProfileName(TEXT("Trigger"));
+	CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &AAlchemyCircleActor::OnOverlapBegin);
+}
+
+void AAlchemyCircleActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AItem* Ingredient = Cast<AItem>(OtherActor);
+	if (!Ingredient)
+		return;
+
+	//if(Ingredient)
+}
--- Panacea/Source/Panacea/AlchemyCircleActor.h
+++ Panacea/Source/Panacea/AlchemyCircleActor.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include "Components/SphereComponent.h"
+
 #include "CoreMinimal.h"
 #include "Item.h"
 #include "AlchemyCircleActor.generated.h"
@@ -14,4 +16,18 @@ class PANACEA_API AAlchemyCircleActor : public AItem
 {
 	GENERATED_BODY()
 	
+public:
+	AAlchemyCircleActor();
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* CollisionComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	FString IngredientNameToPlace;
+
+	// Called when the sphere overlaps with another actor
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+		class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 };
