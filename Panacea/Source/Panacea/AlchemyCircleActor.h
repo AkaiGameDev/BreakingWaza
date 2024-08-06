c2fe23c597fcf9335e748a4d8cd7447a9abe00d2
Oleksandr Smirnov <116755622+oophiuchuss@users.noreply.github.com>
2024-08-06 10:22:54 +0200

    Started implementing

---
+++ Panacea/Source/Panacea/AlchemyCircleActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "AlchemyCircleActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PANACEA_API AAlchemyCircleActor : public AItem
+{
+	GENERATED_BODY()
+	
+};
