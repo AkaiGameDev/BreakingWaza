b49242ad09f1bed949a599ece266a68e8b062340
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2024-08-07 11:07:30 +0200

    Delete Pause and move brodcast to close

--- Panacea/Source/Panacea/NoteActor.cpp
+++ Panacea/Source/Panacea/NoteActor.cpp
@@ -18,86 +18,77 @@ void ANoteActor::BeginPlay()
 }
 
 void ANoteActor::Interact()
-{ 
-	UE_LOG(LogTemp, Warning, TEXT("Diary interact called"));
-	if (Interactable) {
-		UE_LOG(LogTemp, Warning, TEXT("Diary interacted with"));
-		Broadcast();
+{
+	if (Interactable)
+	{
+
+		if (bOppened)
+		{
+			if (PauseMenuWidget)
+			{
+				PauseMenuWidget->RemoveFromParent();
+				PauseMenuWidget = nullptr;
+				UE_LOG(LogTemp, Warning, TEXT("Pause Menu Removed"));
+
+				APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+				if (PlayerController)
+				{
+					FInputModeGameOnly InputMode;
+					PlayerController->SetInputMode(InputMode);
+					PlayerController->bShowMouseCursor = false;
+				}
+			}
+
+			bOppened = false;
+			Broadcast();
+		}
+		else
+		{
+			if (!PauseMenuWidgetClass)
+			{
+				UE_LOG(LogTemp, Error, TEXT("PauseMenuWidgetClass is not set"));
+				return;
+			}
+
+			// Get the player controller
+			APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+			if (!PlayerController)
+			{
+				UE_LOG(LogTemp, Error, TEXT("PlayerController is not found."));
+				return;
+			}
+
+			// Create the pause menu widget
+			PauseMenuWidget = CreateWidget<UUserWidget>(PlayerController, PauseMenuWidgetClass);
+			if (!PauseMenuWidget)
+			{
+				UE_LOG(LogTemp, Error, TEXT("PauseMenuWidget could not be created."));
+				return;
+			}
+
+			// Add the widget to the viewport
+			PauseMenuWidget->AddToViewport();
+
+			//get text from the note
+			UTextBlock* TextBlock = Cast<UTextBlock>(PauseMenuWidget->GetWidgetFromName(TEXT("Content")));
+			if (TextBlock)
+			{
+				TextBlock->SetText(FText::FromString(NoteText));
+			}
 
 
+			// Set the input mode to UI only and show the mouse cursor
+			FInputModeUIOnly InputMode;
+			InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 
-		UE_LOG(LogTemp, Warning, TEXT("Dairy function called"));
-		
-		    bool bIsGamePaused = UGameplayStatics::IsGamePaused(GetWorld());
-		    UE_LOG(LogTemp, Warning, TEXT("Is Game Paused: %s"), bIsGamePaused ? TEXT("True") : TEXT("False"));
-		
-		    if (bIsGamePaused)
-		    {
-		        // Unpause the game if it's currently paused
-		        if (PauseMenuWidget)
-		        {
-		            PauseMenuWidget->RemoveFromParent();
-		            PauseMenuWidget = nullptr;
-		            UE_LOG(LogTemp, Warning, TEXT("Pause Menu Removed"));
-		
-		            APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
-		            if (PlayerController)
-		            {
-		                FInputModeGameOnly InputMode;
-		                PlayerController->SetInputMode(InputMode);
-		                PlayerController->bShowMouseCursor = false;
-		            }
-		        }
-		
-		        UGameplayStatics::SetGamePaused(GetWorld(), false);
-		        UE_LOG(LogTemp, Warning, TEXT("Game Unpaused"));
-		    }
-		    else
-		    {
-		
-		            if (!PauseMenuWidgetClass)
-		            {
-		                UE_LOG(LogTemp, Error, TEXT("PauseMenuWidgetClass is not set"));
-		                return;
-		            }
-		
-		            // Get the player controller
-		            APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
-		            if (!PlayerController)
-		            {
-		                UE_LOG(LogTemp, Error, TEXT("PlayerController is not found."));
-		                return;
-		            }
-		
-		            // Create the pause menu widget
-		            PauseMenuWidget = CreateWidget<UUserWidget>(PlayerController, PauseMenuWidgetClass);
-		            if (!PauseMenuWidget)
-		            {
-		                UE_LOG(LogTemp, Error, TEXT("PauseMenuWidget could not be created."));
-		                return;
-		            }
-		
-		            // Add the widget to the viewport
-		            PauseMenuWidget->AddToViewport();
-
-		    		//get text from the note
-		            UTextBlock* TextBlock = Cast<UTextBlock>(PauseMenuWidget->GetWidgetFromName(TEXT("Content")));
-		            if (TextBlock)
-		            {
-		                TextBlock->SetText(FText::FromString(NoteText));
-		            }
+			//add input to input mode
 
 
-		            // Set the input mode to UI only and show the mouse cursor
-		            FInputModeUIOnly InputMode;
-		            InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
-		            PlayerController->SetInputMode(InputMode);
-		            PlayerController->bShowMouseCursor = true;
-		
-		            // Pause the game
-		            UGameplayStatics::SetGamePaused(this, true);
-		            UE_LOG(LogTemp, Warning, TEXT("Game Paused and Pause Menu Displayed"));
-		    }
+			PlayerController->SetInputMode(InputMode);
+			PlayerController->bShowMouseCursor = true;
 
+			bOppened = true;
+
+		}
 	}
 }
--- Panacea/Source/Panacea/NoteActor.h
+++ Panacea/Source/Panacea/NoteActor.h
@@ -28,6 +28,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	bool bOppened = false;
+
 public:
 	virtual void Interact() override;
 };
